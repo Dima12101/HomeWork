ced07c8ec95fb3a7215ada29e0ad9d089f110c04
Терещенко Дмитрий <dima123-99@mail.ru>
2018-02-18 17:36:30 +0300

    2_1_2

---
+++ Semestr_2/Task_1/Number_2/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.Write("Input index: ");
+			int index = Int32.Parse(Console.ReadLine());
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.Write("Result: ");
+			Console.Write(Fibonachi(index));
+			Console.ReadKey();
+		}
+		private static int Fibonachi(int index)
+		{
+			if (index <= 1)
+			{
+				return 1;
+			}
+			else
+			{
+				return Fibonachi(index - 1) + Fibonachi(index - 2);
+			}
+		}
+	}
+}
