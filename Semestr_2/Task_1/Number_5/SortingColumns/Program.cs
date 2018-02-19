a492aff96ccbaee96abe22f78ae5b514a85878a7
Терещенко Дмитрий <dima123-99@mail.ru>
2018-02-19 21:38:07 +0300

    Create

--- Semestr_2/Task_1/Number_5/SortingColumns/Program.cs
+++ Semestr_2/Task_1/Number_5/SortingColumns/Program.cs
@@ -15,8 +15,8 @@ namespace SortingColumns
 			int[,] matrix = new int[countLines, countColumns];
 			Console.ForegroundColor = ConsoleColor.Red;
 			Console.WriteLine("Matrix:");
-			FillingMatrix(matrix, countLines, countColumns);
-			SortingColumns(matrix, countLines, countColumns);
+			FillingMatrix(matrix);
+			SortingColumns(matrix);
 			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine("SortingMatrix:");
 			for (int i = 0; i < countLines; i++)
@@ -31,12 +31,12 @@ namespace SortingColumns
 			Console.ReadKey();
 		}
 
-		static void FillingMatrix (int[,] matrix, int countLines, int countColumns)
+		static void FillingMatrix (int[,] matrix)
 		{
 			Random rand = new Random();
-			for (int i = 0; i < countLines; i++)
+			for (int i = 0; i < matrix.GetLength(0); i++)
 			{
-				for (int j = 0; j < countColumns; j++)
+				for (int j = 0; j < matrix.GetLength(1); j++)
 				{
 					matrix[i, j] = rand.Next(1, 100);
 					Console.Write(matrix[i, j]);
@@ -46,17 +46,17 @@ namespace SortingColumns
 			}
 		}
 
-		static void SortingColumns(int[,] matrix, int countLines, int countColumns)
+		static void SortingColumns(int[,] matrix)
 		{
 			bool stop = false;
 			while (!stop)
 			{
 				stop = true;
-				for (var j = 1; j < countColumns; j++)
+				for (var j = 1; j < matrix.GetLength(1); j++)
 				{
-					if (matrix[0, j] < matrix[0,j - 1])
+					if (matrix[0, j] < matrix[0, j - 1]) 
 					{
-						for (int i = 0; i < countLines; i++)
+						for (int i = 0; i < matrix.GetLength(0); i++)
 						{
 							var a = matrix[i, j];
 							matrix[i, j] = matrix[i, j - 1];
