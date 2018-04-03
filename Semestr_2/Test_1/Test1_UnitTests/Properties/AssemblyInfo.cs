dc8033be003d668bd1567fea1484709be6359eaf
Терещенко Дмитрий <dima123-99@mail.ru>
2018-04-03 16:55:26 +0300

    This is done

---
+++ Semestr_2/Test_1/Test1_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test1_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test1_UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc9df760-d7e3-4404-b156-68071205bec3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Semestr_2/Test_1/Test_1/Queue.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test_1
+{
+	public class Queue<Tvalue>
+	{
+		private class Node
+		{
+			public Tvalue value;
+			public int priority;
+			public Node next;
+			public Node prev;
+		}
+
+		private Node head;
+		private Node tail;
+
+		public bool isEmpty => (head == null || tail == null);
+
+		public void Enqueue(Tvalue _value, int _priority)
+		{
+			if (isEmpty)
+			{
+				head = tail = new Node
+				{
+					value = _value,
+					priority = _priority,
+				};
+			}
+			else
+			{
+				Node temp = tail;
+				while (temp != null && _priority > temp.priority)
+				{
+					temp = temp.prev;
+				}
+				Node newNode = new Node
+				{
+					value = _value,
+					priority = _priority,
+				};
+				if (temp == null)
+				{
+					newNode.next = head;
+					head.prev = newNode;
+					head = newNode;
+				}
+				else if (temp == tail)
+				{
+					newNode.prev = tail;
+					tail.next = newNode;
+					tail = newNode;
+				}
+				else
+				{
+					newNode.prev = temp;
+					newNode.next = temp.next;
+					temp.next.prev = newNode;
+					temp.next = newNode;
+				}
+			}
+		}
+
+		public Tvalue Dequeue()
+		{
+			if (isEmpty)
+			{
+				throw new Exception("Queue is empty!");
+			}
+			Node temp = head;
+			head = temp.next;
+			if (head == null)
+			{
+				tail = null;
+			}
+			else
+			{
+				head.prev = null;
+			}
+			temp.next = null;
+			return temp.value;
+		}
+	}
+}
