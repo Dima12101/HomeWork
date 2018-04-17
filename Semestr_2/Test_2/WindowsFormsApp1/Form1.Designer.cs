43c1e319d7e7e83f06f63d6a372d5f99293731ab
Терещенко Дмитрий <dima123-99@mail.ru>
2018-04-17 17:10:07 +0300

    Not end develompent

---
+++ Semestr_2/Test_2/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class TickTackToe
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button9 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button9
+			// 
+			this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button9.Location = new System.Drawing.Point(227, 187);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(106, 87);
+			this.button9.TabIndex = 8;
+			this.button9.Tag = "33";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.Button_click);
+			this.button9.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button9.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button8
+			// 
+			this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button8.Location = new System.Drawing.Point(115, 187);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(106, 87);
+			this.button8.TabIndex = 7;
+			this.button8.Tag = "32";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.Button_click);
+			this.button8.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button8.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button7
+			// 
+			this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button7.Location = new System.Drawing.Point(3, 187);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(106, 87);
+			this.button7.TabIndex = 6;
+			this.button7.Tag = "31";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.Button_click);
+			this.button7.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button7.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button6
+			// 
+			this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button6.Location = new System.Drawing.Point(227, 95);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(106, 86);
+			this.button6.TabIndex = 5;
+			this.button6.Tag = "23";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.Button_click);
+			this.button6.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button6.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button5
+			// 
+			this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button5.Location = new System.Drawing.Point(115, 95);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(106, 86);
+			this.button5.TabIndex = 4;
+			this.button5.Tag = "22";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button_click);
+			this.button5.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button5.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button4
+			// 
+			this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button4.Location = new System.Drawing.Point(3, 95);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(106, 86);
+			this.button4.TabIndex = 3;
+			this.button4.Tag = "21";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button_click);
+			this.button4.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button4.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button3
+			// 
+			this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button3.Location = new System.Drawing.Point(227, 3);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(106, 86);
+			this.button3.TabIndex = 2;
+			this.button3.Tag = "13";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button_click);
+			this.button3.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button3.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button2
+			// 
+			this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button2.Location = new System.Drawing.Point(115, 3);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(106, 86);
+			this.button2.TabIndex = 1;
+			this.button2.Tag = "12";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button_click);
+			this.button2.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button2.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button1
+			// 
+			this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button1.Location = new System.Drawing.Point(3, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(106, 86);
+			this.button1.TabIndex = 0;
+			this.button1.Tag = "11";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button_click);
+			this.button1.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button1.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 4;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.22222F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.22222F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.22222F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+			this.tableLayoutPanel1.Controls.Add(this.button4, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.button5, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.button6, 2, 1);
+			this.tableLayoutPanel1.Controls.Add(this.button7, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.button8, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.button9, 2, 2);
+			this.tableLayoutPanel1.Controls.Add(this.listView1, 3, 2);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(508, 277);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// listView1
+			// 
+			this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listView1.Location = new System.Drawing.Point(339, 187);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(166, 87);
+			this.listView1.TabIndex = 9;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.SmallIcon;
+			// 
+			// TickTackToe
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(508, 277);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "TickTackToe";
+			this.Text = "TickTackToe";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.ListView listView1;
+	}
+}
+
